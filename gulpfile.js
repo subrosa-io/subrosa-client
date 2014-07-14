3b75f31349a4a833ef4c3b43c5de2c2cf2e853ee
rschaosid <rschaosid@gmail.com>
2014-07-13 23:25:40 -0600

    minify html too

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,10 @@ var jshintStylish = require('jshint-stylish');
 var usemin = require('gulp-usemin');
 var minifyCss = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
+var minifyHtml = require('gulp-minify-html');
 var merge = require('merge-stream');
+var util = require('util');
+var Stream = require('stream');
 
 gulp.task('clean', function(cb) {
   return del(['dist'], cb);
@@ -19,16 +22,62 @@ gulp.task('lint', function() {
 });
 
 gulp.task('build', ['clean'], function() {
-  var app = gulp.src('src/index.html')
+  var useminStream = gulp.src('src/index.html')
     .pipe(usemin({
       'css': ['concat', minifyCss()],
       'app-js': ['concat', uglify()],
       'vendor-js': ['concat', uglify()]
     }))
-    .pipe(gulp.dest('dist'));
+    .pipe(reseparateUseminOutput());
   
-  var static = gulp.src(['src/fonts/*', 'src/img/**/*', 'src/sound/*'], { base: 'src' })
-    .pipe(gulp.dest('dist'));
+  var document = useminStream.document
+    .pipe(minifyHtml());
+  var assets = useminStream.assets;
   
-  return merge(app, static);
+  var static = gulp.src(['src/fonts/*', 'src/img/**/*', 'src/sound/*'], { base: 'src' });
+  
+  return merge(document, assets, static)
+    .pipe(gulp.dest('dist'));
 });
+
+/*
+ * gulp-usemin stupidly dumps its asset files into the main stream. This
+ * separates index.html from the minified asset files in order to html-minify
+ * index.html without trying to html-minify the asset files, which obviously
+ * doesn't work.
+ */
+
+function reseparateUseminOutput() {
+  var ret = new Stream.Writable({ objectMode: true });
+  ret.document = new Stream.Readable({ objectMode: true });
+  ret.assets = new Stream.Readable({ objectMode: true });
+  util.inherits(ret, Stream.Writable);
+  util.inherits(ret.document, Stream.Readable);
+  util.inherits(ret.assets, Stream.Readable);
+  var ready = null;
+  ret._write = function(file, _, cb) {
+    var dest;
+    if (file === null) {
+      ret.document.push(null);
+      ret.assets.push(null);
+      return cb();
+    }
+    
+    if (file.path === 'index.html')
+      { dest = 'document'; }
+    else
+      { dest = 'assets'; }
+    
+    if (ret[dest].push(file))
+      { cb(); ready = null; }
+    else
+      { ready = cb; }
+  };
+  ret.document._read = ret.assets._read = function() {
+    if (ready) {
+      ready();
+      ready = null;
+    }
+  };
+  return ret;
+}
