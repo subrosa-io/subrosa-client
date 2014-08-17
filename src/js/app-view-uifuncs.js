b1d0180282299007de88c3275bd63b9e88c6dac5
ixro <contact@subrosa.io>
2014-08-18 00:18:19 +1000

    Prevent backspace from navigating back.

--- src/js/app-view-uifuncs.js
+++ src/js/app-view-uifuncs.js
@@ -241,3 +241,29 @@ function stopTitleAlert(){
 	clearInterval(titleAlertInterval);
 	document.title = originalTitle;
 }
+// Prevent the backspace key from navigating back.
+$(document).bind('keydown', function (event) {
+    var doPrevent = false;
+    if (event.keyCode === 8) {
+        var d = event.srcElement || event.target;
+        if ((d.tagName.toUpperCase() === 'INPUT' && 
+             (
+                 d.type.toUpperCase() === 'TEXT' ||
+                 d.type.toUpperCase() === 'PASSWORD' || 
+                 d.type.toUpperCase() === 'FILE' || 
+                 d.type.toUpperCase() === 'EMAIL' || 
+                 d.type.toUpperCase() === 'SEARCH' || 
+                 d.type.toUpperCase() === 'DATE' )
+             ) || 
+             d.tagName.toUpperCase() === 'TEXTAREA') {
+            doPrevent = d.readOnly || d.disabled;
+        }
+        else {
+            doPrevent = true;
+        }
+    }
+
+    if (doPrevent) {
+        event.preventDefault();
+    }
+});
